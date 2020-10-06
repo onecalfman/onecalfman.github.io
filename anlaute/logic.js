bc04322ce87dad3e4904448ed6dfc12a4d781265
onecalfman <jonas@artograph.info>
2020-10-06 23:30:46 +0200

    mostly finished

--- anlaute/logic.js
+++ anlaute/logic.js
@@ -18,9 +18,9 @@ ctx.font = FONT_SIZE + 'px sans';
 ctx.textBaseline = "hanging";
 
 var cards = [];
-var images = []
+var csv = []
 
-colors = [
+colors = [ 
 	'#86C9B7',
 	'#87A7C7',
 	'#94D0A1',
@@ -41,6 +41,7 @@ colors = [
 
 var mousedown = false;
 var movestart = [0,0]
+for (let i = 0; i <= CARDS_N; i++) { csv.push([]); }
 
 window.mobileCheck = function() { let check = false; (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera); return check; };
 
@@ -49,7 +50,6 @@ if (window.mobileCheck()) {
 	ctx.font = FONT_SIZE + 'px sans';
 	var SCALE = 0.38;
 	var IMG_SCALE = 0.38;
-	console.log('is Mobile');
 }
 
 
@@ -132,7 +132,6 @@ function unite(p)
 		cards[p].add(a);
 	}
 	else {
-		console.log('new united');
 		if ( cards[a].w <= cards[p].w) { w = cards[p].w; } else { w = cards[a].w; }
 		if ( cards[a].h <= cards[p].h) { h = cards[p].h; } else { h = cards[a].h; }
 		u = new United(cards[a].group, cards[a].x, cards[a].y, w, h);
@@ -163,10 +162,6 @@ function match()
 			return;
 		}
 	}
-	console.log(movestart[0]);
-	console.log(card.x);
-	console.log(movestart[1]);
-	console.log(card.y);
 	if ( card.x === movestart[0] && card.y === movestart[1] ) {
 		if ( card instanceof SoundCard ) { card.play(); };
 		if ( card instanceof United && card.snd ) { card.play(); };
@@ -230,8 +225,8 @@ class ImageCard {
 	constructor(group, img) {
 	this.group = group;
 	this.img = img;
-	this.h = images[group].height * IMG_SCALE;
-	this.w = images[group].width * IMG_SCALE;
+	this.h = this.img.height * IMG_SCALE;
+	this.w = this.img.width * IMG_SCALE;
 	this.x = randInt(0, canvas.width - this.w);
 	this.y = randInt(0, canvas.height - this.h);
 	}
@@ -269,7 +264,7 @@ class SoundCard {
 	this.w = 100 * SCALE;
 	this.x = randInt(0, canvas.width - this.w);
 	this.y = randInt(0, canvas.height - this.h);
-	this.snd = new Audio(snd);
+	this.snd = snd;
 	this.color = randPred();
 	}
 
@@ -323,8 +318,6 @@ class United {
 		for ( let i = 0; i < imgl; i++) {
 			if ( imgl === 1 ) { 
 				var offsetX = this.w / 2 - this.img[0].width * IMG_SCALE / 2;
-				console.log('offsetX');
-				console.log('offsetY');
 				var offsetY = this.h / 2 - this.img[0].height * IMG_SCALE / 2;
 			} else {
 				var offsetX = i * this.w / 2 + 0.07 * this.w;
@@ -371,24 +364,25 @@ function init()
 		n = randInt(0,lines.length - 1);
 		c = randInt(0,colors.length - 1);
 		cells = lines[n].split(',');
-		lines.splice(n,1);
-		colors.splice(c,1);
 		for( let j = 0; j < cells.length; j++)
 		{
-			if ( cells[j].endsWith('png') || cells[j].endsWith('PNG')) {
-			images[i] = new Image();
-			images[i].onload = function() { cards.push(new ImageCard(i,images[i])); draw();}
-			images[i].src = cells[j];
+			if ( cells[j].endsWith('png') || cells[j].endsWith('PNG') || cells[j].endsWith('jpg') || cells[j].endsWith('JPG')) {
+			csv[i][j] = new Image();
+			csv[i][j].onload = function() { cards.push(new ImageCard(i,csv[i][j])); draw();}
+			csv[i][j].src = cells[j];
 			} 
-			else if  ( cells[j].endsWith('mp3') || cells[j].endsWith('wav') || cells[j].endsWith('m4a'))
-			{
-			cards.push(new SoundCard(i,cells[j]));
+			else if  ( cells[j].endsWith('mp3') || cells[j].endsWith('wav'))
+			{ 
+				csv[i][j] = new Audio(cells[j]);
+				cards.push(new SoundCard(i,csv[i][j]));
 			} 
-			else 
-			{
-			cards.push(new TextCard(i,cells[j]));
+			else {
+				csv[i][j] = cells[j];
+				cards.push(new TextCard(i,csv[i][j]));
 			}
 		}
+		lines.splice(n,1);
+		colors.splice(c,1);
 	}
 
 	draw();
@@ -400,6 +394,5 @@ function init()
     	canvas.addEventListener("touchend",  function(event) {mousedown = false; match();});
 
 	window.addEventListener("resize", function(event) {resize();}, true);
-	//window.addEventListener("deviceorientation", rotate(event), true);
 	draw();
 }
