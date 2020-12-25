5447ef42dae0f66ba916cb7392f61c6716929332
onecalfman <jonas@artograph.info>
2020-12-25 15:00:22 +0100

    feld 1.2

--- feld/logic.js
+++ feld/logic.js
@@ -5,12 +5,16 @@ var FONT = "Grundschrift";
 var FONT_SIZE = 50;
 var CARDS = [];
 
+var time = 60;
+var guiHeight;
 var lines = 7;
 var rows = 7;
 var lineWidth;
 var rowHeight;
 
 var maxBoardSize = 600;
+var alpha = 1;
+var countdown = '';
 
 var colors = [ 
 	'#86C9B7', '#87A7C7', '#94D0A1', '#8ECC85',
@@ -33,6 +37,7 @@ const par = new URLSearchParams(window.location.search);
 if ( par.get('d')) 		{ lines = par.get('d'); rows = lines;}
 if ( par.get('l')) 		{ lines = par.get('l');}
 if ( par.get('r')) 		{ rows = par.get('d');}
+if ( par.get('t')) 		{ rows = par.get('t');}
 
 function initSelect(event) {
 	select({ x: event.touches[0].clientX, y : event.touches[0].clientY });
@@ -75,13 +80,36 @@ function checkSelection() {
 }
 
 function draw() {
+	ctx.font = FONT_SIZE + 'px ' + FONT;
+	ctx.globalAlpha = alpha;
 	ctx.fillStyle = bgColor;
 	ctx.fillRect(0,0,canvas.width, canvas.height);
 	fall();
 	for(i in cards) {
 		cards[i].draw(); 
 	}
+	ctx.globalAlpha = 1;
+	ctx.font = guiHeight * 0.8 + 'px ' + FONT;
+	ctx.fillText(time, canvas.width / 2, canvas.height / 12);
+	ctx.font = '180px ' + FONT;
+	ctx.fillText(countdown, canvas.width / 2, (canvas.height + guiHeight) / 2);
 }
+
+function timer() {
+	time--;
+}
+
+function checkCollision() {
+	for(i in cards) {
+		for(j in cards) {
+			if ( j == i ) { continue; }
+			if ( Match.card(cards[j].x, cards[j].y, cards[i])) {
+				cards[j].y = -10;
+			}
+		}
+	}
+}
+
 function fall() {
 	for(i in cards) {
 		let collision = false;
@@ -106,9 +134,10 @@ function fall() {
 		}
 	}
 }
+
 function createCards() {
 	lineWidth = Math.round(canvas.width / lines);
-	lineHeight = Math.round(canvas.height / rows);
+	lineHeight = Math.round((canvas.height - guiHeight) / rows);
 	for(let i = 0; i < lines; i++ ) {
 		for(let j = 0; j < rows; j++) {
 			card = new Card(randInt(1,10), lineWidth, lineHeight);
@@ -132,13 +161,27 @@ async function init() {
 	ctx.canvas.height = window.innerHeight;
 	ctx.fillStyle = bgColor;
 	ctx.fillRect(0,0,canvas.width,canvas.height);
-	ctx.globalAlpha = 1;
+	alpha = 0.4;
 	ctx.font = FONT_SIZE + 'px ' + FONT;
 	ctx.textBaseline = "middle";
 	ctx.textAlign = "center";
+	guiHeight = canvas.height / 6;
 	createCards();
 	sleep(1);
 	setInterval(draw, 30);
+
+	setTimeout( () => { countdown = 3 }, 2000);
+	setTimeout( () => {countdown = 2 }, 3000);
+	setTimeout( () => {countdown = 1 }, 4000);
+	setTimeout( () => {
+		countdown = 'LOS!';
+		alpha = 1;
+		setInterval(timer, 1000);
+	}, 5000);
+	setTimeout( () => {countdown = '' }, 5500);
+
+	setTimeout( () => setInterval(checkCollision, 3000), 2000);
+
 
 	canvas.addEventListener("touchstart", initSelect, false);
 	canvas.addEventListener("mousedown", select, false);
