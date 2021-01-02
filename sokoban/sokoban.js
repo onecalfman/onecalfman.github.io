e1ff21ff3726077bed7d6a73c05debf33ed8038a
onecalfman <jonas@artograph.info>
2021-01-02 22:47:25 +0100

    show numbers

--- sokoban/sokoban.js
+++ sokoban/sokoban.js
@@ -2,12 +2,15 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-var bgColor = '#ddd';
+var bgColor = '#ccbd97';
 var FONT = "Grundschrift";
 var FONT_SIZE;
 
 var cards = [];
 var buttons = [];
+var cheeses = [];
+var backgroundCards = [];
+var task;
 
 var grid;
 var level = [];
@@ -20,9 +23,15 @@ var playerImg = new Image();
 playerImg.src = 'assets/player.png';
 var cheese = new Image();
 cheese.src = 'assets/cheese.png';
-var wall = [new Image(), new Image()];
+var wall = [new Image(), new Image(), new Image(), new Image()];
 wall[0].src = 'assets/pflanze.png'
-wall[1].src = 'assets/ananaspflanze.png'
+wall[1].src = 'assets/pflanze2.png'
+wall[2].src = 'assets/ananaspflanze.png'
+wall[3].src = 'assets/felsen.png'
+var target = new Image();
+target.src = 'assets/ziel.png';
+var background = new Image();
+background.src = 'assets/bg.png';
 
 var colors = [ 
 	'#86C9B7', '#87A7C7', '#94D0A1', '#8ECC85',
@@ -37,17 +46,23 @@ if ( par.get('l')) { n = par.get('l');}
 function draw() {
 	ctx.fillStyle = bgColor;
 	ctx.fillRect(0,0,canvas.width, canvas.height);
+	for(i in backgroundCards) { backgroundCards[i].draw(); }
 	for(i in cards) { cards[i].draw(); }
 	for(i in buttons) { buttons[i].draw(); }
+	for(i in cheeses) { cheeses[i].draw(); }
 	player.draw();
 }
 
 
 function collision(card) {
-	possibleCards = 'xo';
+	let solid = 'xo';
+	let target = 'g';
 	for(i in cards) {
 		if(cards[i] === card) { continue; }
-		if(card.x === cards[i].x && card.y === cards[i].y && possibleCards.includes(cards[i].group)) { return i; }
+		if(card.x === cards[i].x && card.y === cards[i].y) {
+			if (solid.includes(cards[i].group)) { return [1, i]; }
+			else { return [2, i]; }
+		}
 	}
 	return false;
 }
@@ -87,7 +102,8 @@ function move(event) {
 		case 40 : player.y += grid; player.orientation = 'd';	break;
 	}
 	let i = collision(player);
-	if(i) {
+	if(i[0] == 1) {
+		i = i[1];
 		switch(cards[i].group) {
 			case 'x' : 
 				player.x = pos.x;
@@ -98,7 +114,8 @@ function move(event) {
 				cards[i].x += player.x - pos.x;
 				cards[i].y += player.y - pos.y;
 				let j = collision(cards[i]);
-				if(j) {
+				if(j[0] == 1) {
+					j = j[1];
 					if ( cards[j].group == 'g' ) {
 						cards[i].color = randPred();
 						cards[i].alpha = 1;
@@ -112,57 +129,16 @@ function move(event) {
 				break;
 		}
 	}
+
 	draw();
 	setTimeout(() => { document.addEventListener('keydown', move); }, 50);
 }
-
-//function createLevel() {
-//	for(let r = 0; r < levelHeight; r++) {
-//		for(let c = 0; c < level[r].length; c++) {
-//			card = new Card(level[r][c],grid,grid);
-//			switch(level[r][c]) {
-//				case 'x': card.color = '#666'; break;
-//				case 'o': card.color = '#996'; break;
-//				case 'p': card.color = '#696'; break;
-//				case 'g': card.color = '#111'; break;
-//				case ' ':   continue; 	       break;
-//				default : card.color = '#449'; break;
-//			}
-//			card.x = Math.round(grid / 2 + grid * c);
-//			card.y = Math.round(grid / 2 + grid * r);
-//			if ( level[r][c] == 'p') {
-//				player = card;	
-//				player.alpha = 1;
-//				player.color = false;
-//				player.orientation = 'u';
-//				playerImg = new Image();
-//				playerImg.onload = function() { 
-//					player.img[0] = playerImg;
-//					player.draw = function() {
-//						let rad = 0;
-//						switch(this.orientation) {
-//							case 'u': rad = 0; 		break;
-//							case 'r': rad = Math.PI / 2; 	break;
-//							case 'd': rad = Math.PI; 	break;
-//							case 'l': rad = Math.PI * 1.5;  break;
-//						}
-//						PlaceImg.rotate(this.img[0], this.x, this.y, this.w, this.h, rad);
-//					}
-//					draw();
-//				};
-//				playerImg.src = 'assets/player.png';
-//			}
-//			cards.push(card);
-//		}
-//	}
-//	draw();
-//}
 
 function addCheese(card,c,r) {
 	card.x = Math.round(grid / 2 + grid * c);
 	card.y = Math.round(grid / 2 + grid * r);
 	card.img[0] = cheese;
-	card.txt[0] = randInt(1,9);
+	card.txt[0] = task[cheeses.length];
 	card.draw = function() {
 		PlaceImg.center(this.img[0], this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
 		ctx.fillStyle = '#333';
@@ -171,7 +147,21 @@ function addCheese(card,c,r) {
 		ctx.textBaseline = 'middle';
 		ctx.fillText(card.txt[0], this.x, this.y+this.h*0.15);
 	}
+	cheeses.push(card);
 	cards.push(card);
+}
+
+function createBackground() {
+	for(let r = 0; r <= Math.ceil(canvas.height/grid); r+=3) {
+		for(let c = 0; c <= Math.ceil(canvas.width/grid); c+=3) {
+			log('card');
+			bg_card = new Card('b',3*grid,3*grid);
+			bg_card.img[0] = background;
+			bg_card.x = Math.round(grid / 2 + grid * c);
+			bg_card.y = Math.round(grid / 2 + grid * r);
+			backgroundCards.push(bg_card);
+		}
+	}
 }
 
 function createLevel() {
@@ -182,7 +172,7 @@ function createLevel() {
 				case 'x': card.img[0] = wall[randInt(0,1)]; break;
 				case 'o': addCheese(card,c,r); continue; break;
 				case 'p': card.img[0] = playerImg; break;
-				case 'g': card.color = '#111'; break;
+				case 'g': card.img[0] = target; break;
 				case ' ':   continue; 	       break;
 				default : card.color = '#449'; break;
 			}
@@ -204,13 +194,16 @@ function createLevel() {
 					PlaceImg.rotate(this.img[0], this.x, this.y, this.w, this.h, rad);
 				}
 			}
-		cards.push(card);
+		if ( card.group == 'g' ) { cards.unshift(card); }
+		else { cards.push(card); }
 		}
 	}
 	draw();
 }
 
 function parseLevel(text) {
+	task = text.split('#')[1].replaceAll('\n','').replaceAll(' ','');
+	text = text.split('#')[0];
 	level.push([]);
 	let row = 0;
 	for(i in text) {
@@ -236,6 +229,7 @@ function initLevel(n) {
 			grid = Math.round(Math.min(canvas.width/levelWidth, canvas.height/levelHeight));
 			FONT_SIZE = grid * 0.8;
 			if(isTouchDevice) { touchControlls(); }
+			createBackground();
 			createLevel();
 		});
 	});
